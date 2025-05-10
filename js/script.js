fda9f29488c74cb3ee6c4091ed56b8798969af5d
Raudhah Maszamanie <152466829+wannurraudhah@users.noreply.github.com>
2025-05-11 03:15:53 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,7 @@
 const scrollBtn = document.getElementById('scrollTop');
-
 window.addEventListener('scroll', () => {
   scrollBtn.style.display = window.scrollY > 200 ? 'flex' : 'none';
 });
-
 scrollBtn.addEventListener('click', () => {
   window.scrollTo({ top: 0, behavior: 'smooth' });
 });
