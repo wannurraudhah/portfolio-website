73ef9f8e38a4b076537499a8d5e535175a9101e5
Raudhah Maszamanie <152466829+wannurraudhah@users.noreply.github.com>
2025-05-11 02:50:26 +0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const scrollBtn = document.getElementById('scrollTop');
+
+window.addEventListener('scroll', () => {
+  scrollBtn.style.display = window.scrollY > 200 ? 'flex' : 'none';
+});
+
+scrollBtn.addEventListener('click', () => {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+});
